675b8c57837932a3aa00e311714efc89d5b66ef9
Philipp Meschenmoser <Philipp.Meschenmoser@uni.kn>
2017-12-06 11:36:29 +0100

    push weather data

--- src/com/alba/Main.java
+++ src/com/alba/Main.java
@@ -6,12 +6,229 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	    //insertAlbatrosses(new File("d/alba.csv"));
-        insertLanduse(new File("d/landuse.csv"));
+        //insertMontes("Capitan Montes", new File("d/weather/CapitanMontes.csv"));
+        //insertEloy( "Eloy Alfaro", new File("d/weather/EloyAlfaroInternational.csv"));
+        //insertPedro("Pedro Canga", new File("d/weather/PedroCanga.csv"));
+        //insertSanCristobal("San Cristobal", new File("d/weather/SanCristobal.csv"));
+        //insertAlbatrosses(new File("d/alba.csv"));
+        //insertLanduse(new File("d/landuse.csv"));
+        insertJose("Jose", new File("d/weather/Jose.csv"));
     }
+
+    private static void insertMontes(String name,  File f){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line;
+            Connection c = getConnection();
+            PreparedStatement ps = c.prepareStatement("INSERT INTO weather VALUES (?, ?::timestamp without time zone, ?, ? , ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING");
+            int count = 0;
+            int total = 1;
+            while ((line = br.readLine()) != null) {
+                System.out.println(total);
+                total++;
+                String[] fields = line.split(",");
+                if (!fields[8].equals("Calm")){
+                    ps.setString(1, name); //location
+                    ps.setString(2, fields[0]); //timestamp
+                    ps.setDouble(3, getDouble(fields[1])); //temperature
+                    ps.setDouble(4, getDouble(fields[3])); //dew point
+                    ps.setInt(5, getInteger(fields[4])); //humidity
+                    ps.setInt(6, getInteger(fields[5])); //air pressure
+                    ps.setDouble(7, getDouble(fields[6])); //visibility
+                    ps.setString(8, fields[7]); //wind dir
+                    ps.setDouble(9, getDouble(fields[8])); //wind speed
+                    ps.setString(10, fields[9]); //situation
+                    ps.addBatch();
+                }
+                if (count == 100){
+                    ps.executeBatch();
+                    count = 0;
+                }
+                count++;
+            }
+            ps.executeBatch();
+            ps.close();
+            c.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertPedro(String name,  File f){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line;
+            Connection c = getConnection();
+            PreparedStatement ps = c.prepareStatement("INSERT INTO weather VALUES (?, ?::timestamp without time zone, ?, ? , ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING");
+            int count = 0;
+            int total = 1;
+            while ((line = br.readLine()) != null) {
+                System.out.println(total);
+                total++;
+                String[] fields = line.split(",");
+                if (!fields[8].equals("Calm") && fields.length>9){
+                    ps.setString(1, name); //location
+                    ps.setString(2, fields[0]); //timestamp
+                    ps.setDouble(3, getDouble(fields[1])); //temperature
+                    ps.setDouble(4, getDouble(fields[3])); //dew point
+                    ps.setInt(5, getInteger(fields[4])); //humidity
+                    ps.setInt(6, getInteger(fields[5])); //air pressure
+                    ps.setDouble(7, getDouble(fields[6])); //visibility
+                    ps.setString(8, fields[7]); //wind dir
+                    ps.setDouble(9, getDouble(fields[8])); //wind speed
+                    ps.setString(10, fields[9]); //situation
+                    ps.addBatch();
+                    if (count == 100){
+                        ps.executeBatch();
+                        count = 0;
+                    }
+                }
+
+                count++;
+            }
+            ps.executeBatch();
+            ps.close();
+            c.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void insertEloy(String name,  File f){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line;
+            Connection c = getConnection();
+            PreparedStatement ps = c.prepareStatement("INSERT INTO weather VALUES (?, ?::timestamp without time zone, ?, ? , ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING");
+            int count = 0;
+            int total = 1;
+            while ((line = br.readLine()) != null) {
+                System.out.println(total);
+                total++;
+                String[] fields = line.split(",");
+                if (!fields[1].equals("-")) {
+                    ps.setString(1, name); //location
+                    ps.setString(2, fields[0]); //timestamp
+                    ps.setDouble(3, getDouble(fields[1])); //temperature
+                    ps.setDouble(4, getDouble(fields[2])); //dew point
+                    ps.setInt(5, getInteger(fields[3])); //humidity
+                    ps.setInt(6, getInteger(fields[4])); //air pressure
+                    ps.setDouble(7, getDouble(fields[5])); //visibility
+                    ps.setString(8, fields[6]); //wind dir
+                    ps.setDouble(9, getDouble(fields[7])); //wind speed
+                    ps.setString(10, fields[8]); //situation
+                    ps.addBatch();
+                }
+                if (count == 100){
+                    ps.executeBatch();
+                    count = 0;
+                }
+                count++;
+            }
+            ps.executeBatch();
+            ps.close();
+            c.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertSanCristobal(String name,  File f){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line;
+            Connection c = getConnection();
+            PreparedStatement ps = c.prepareStatement("INSERT INTO weather VALUES (?, ?::timestamp without time zone, ?, ? , ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING");
+            int count = 0;
+            int total = 1;
+            while ((line = br.readLine()) != null) {
+                System.out.println(total);
+                total++;
+                String[] fields = line.split(",");
+                if (!fields[1].equals("-")) {
+                    ps.setString(1, name); //location
+                    ps.setString(2, fields[0]); //timestamp
+                    ps.setDouble(3, getDouble(fields[1])); //temperature
+                    ps.setDouble(4, getDouble(fields[2])); //dew point
+                    ps.setInt(5, getInteger(fields[3])); //humidity
+                    ps.setInt(6, getInteger(fields[4])); //air pressure
+                    ps.setDouble(7, getDouble(fields[5])); //visibility
+                    ps.setString(8, fields[6]); //wind dir
+                    ps.setDouble(9, getDouble(fields[7])); //wind speed
+                    ps.setString(10, fields[8]); //situation
+                    ps.addBatch();
+                }
+                if (count == 100){
+                    ps.executeBatch();
+                    count = 0;
+                }
+                count++;
+            }
+            ps.executeBatch();
+            ps.close();
+            c.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertJose(String name,  File f){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line;
+            Connection c = getConnection();
+            PreparedStatement ps = c.prepareStatement("INSERT INTO weather VALUES (?, ?::timestamp without time zone, ?, ? , ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING");
+            int count = 0;
+            int total = 1;
+            while ((line = br.readLine()) != null) {
+                System.out.println(total);
+                total++;
+                String[] fields = line.split(",");
+                if (!fields[1].equals("-") && !fields[2].equals("-") && fields.length == 9 && !fields[7].equals("Calm")) {
+                    ps.setString(1, name); //location
+                    ps.setString(2, fields[0]); //timestamp
+                    ps.setDouble(3, getDouble(fields[1])); //temperature
+                    ps.setDouble(4, getDouble(fields[2])); //dew point
+                    ps.setInt(5, getInteger(fields[3])); //humidity
+                    ps.setInt(6, getInteger(fields[4])); //air pressure
+                    ps.setDouble(7, getDouble(fields[5])); //visibility
+                    ps.setString(8, fields[6]); //wind dir
+                    ps.setDouble(9, getDouble(fields[7])); //wind speed
+                    ps.setString(10, fields[8]); //situation
+                    ps.addBatch();
+                }
+                if (count == 100){
+                    ps.executeBatch();
+                    count = 0;
+                }
+                count++;
+            }
+            ps.executeBatch();
+            ps.close();
+            c.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private static void insertLanduse(File f){
         BufferedReader br = null;
@@ -21,6 +238,7 @@ public class Main {
             PreparedStatement ps = c.prepareStatement("INSERT INTO landuse VALUES (?, ?, ?) ON CONFLICT DO NOTHING");
             int count = 0;
             br = new BufferedReader(new FileReader(f));
+
             while ((line = br.readLine()) != null) {
                 String[] fields = line.split(",");
                 ps.setDouble(1, getDouble(fields[0]));
@@ -31,6 +249,7 @@ public class Main {
                     ps.executeBatch();
                     count = 0;
                 }
+                count++;
             }
             ps.executeBatch();
             ps.close();
@@ -84,13 +303,17 @@ public class Main {
     }
 
     private static double getDouble(String s){
-        s = s.replace("\"","");
+        s = s.replaceAll("\"","");
+        s = s.replaceAll("-", "");
+        s = s.replaceAll("NA","");
         if (s.length()<1) s="-999999.0";
         return Double.parseDouble(s);
     }
 
     private static int getInteger(String s){
         s = s.replaceAll("\"","");
+        s = s.replaceAll("-", "");
+        s = s.replaceAll("NA","");
         if (s.length()<1) s="-999999";
         return Integer.parseInt(s);
     }
