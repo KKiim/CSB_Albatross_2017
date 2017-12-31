7308fa343e4b312ade5f744e9fd324ec6ba95d3f
meschenmoser <philipp.meschenmoser@uni.kn>
2017-12-31 17:11:04 +0100

    line width bug fix

--- alba/js/app/misc/DrawHelper.js
+++ alba/js/app/misc/DrawHelper.js
@@ -293,7 +293,7 @@ var DrawHelper = (function() {
                                 depthTest : {
                                     enabled : true
                                 },
-                                lineWidth : Math.min(this.strokeWidth || 4.0, context._aliasedLineWidthRange[1])
+                                lineWidth : 4
                             }
                         })
                     });
